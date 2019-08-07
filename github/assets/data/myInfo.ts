1f19cad78102cf7e3235102fa6bbabe1149f7108
JoshAgboola <44736801+Josh-Agboola@users.noreply.github.com>
2019-08-07 01:02:55 -0400

    Add files via upload

---
+++ github/assets/data/myInfo.ts
@@ -0,0 +1,9 @@
+import { Agboolaj } from '../../app/agboolaj';
+
+export const MYINFO: Agboolaj = {
+    sid: 991492965,
+    name: 'Joshua Agboola',
+    lName: 'agboolaj',
+    campus: 'Davis Campus',
+    aTitle: 'Assignment 4'
+};
